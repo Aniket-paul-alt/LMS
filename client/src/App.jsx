aa7ab17ec0d81ca626572aacb36a89beaee7bf04
Aniket-paul-alt <113275960+Aniket-paul-alt@users.noreply.github.com>
2025-01-15 20:30:06 +0530

    built course detail page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,7 @@ import CourseTable from './pages/admin/course/CourseTable'
 import EditCourse from './pages/admin/course/EditCourse'
 import CreateLecture from './pages/admin/lecture/CreateLecture'
 import EditLecture from './pages/admin/lecture/EditLecture'
+import CourseDetail from './pages/student/CourseDetail'
 
 const appRouter = createBrowserRouter([
   {
@@ -38,6 +39,10 @@ const appRouter = createBrowserRouter([
       {
         path:"profile",
         element: <Profile/>
+      },
+      {
+        path:"course-detail/:courseId",
+        element: <CourseDetail/>
       },
 
       //admin routes
