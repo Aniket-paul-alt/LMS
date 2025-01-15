fdef588fdb7869c75356793c853d04eb601eb5c7
Aniket-paul-alt <113275960+Aniket-paul-alt@users.noreply.github.com>
2025-01-15 19:45:27 +0530

    get published courses and rendered to the home page done

--- server/controllers/course.controller.js
+++ server/controllers/course.controller.js
@@ -34,6 +34,28 @@ export const createCourse = async(req, res)=>{
     }
 }
 
+export const getPublishedCourse = async(req, res)=>{
+    try {
+        const courses = await Course.find({isPublished: true}).populate({path:"creator", select:"name photoUrl"})
+        if(!courses){
+            return res.status(404).json({
+                success:false,
+                message: "Courses not found"
+            })
+        }
+
+        return res.status(200).json({
+            courses
+        })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            success:false,
+            message: "Failed to get published courses"
+        })
+    }
+}
+
 export const getCreatorCourses = async(req, res)=>{
     try {
         const userId = req.id
